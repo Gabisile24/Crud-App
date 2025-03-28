13a136a6ff25f3a90fa827a9f2d635f35fc679a3
Gabisile24 <B00165995@mytudublin.ie>
2025-03-28 15:09:04 +0000

    Delete

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host     = "localhost";
-$username = "root";
-$password = "MySql@2024";
-$dbname   = "test";
-$dsn      = "mysql:host=$host;dbname=$dbname";
-$options  = array(
-PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-);
